db41ba088fbcd31ed4810f588bff8e425af7926d
CrouchingTiger28 <sabrinatj2004@gmail.com>
2026-02-27 15:34:58 -0700

    Implemented UserService and added AlreadyTaken and InvalidLogin exceptions

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,33 @@
 package service;
 
+import Model.AuthData;
+import Model.UserData;
+
+import java.util.Objects;
+
 public class UserService {
+    dataaccess.UserAccess users = new dataaccess.UserAccess();
+    public UserService() {
+
+    }
+
+    public UserData register(UserData registerRequest) throws AlreadyTakenException {
+        UserData user = users.getUser(registerRequest.username());
+        if (user != null) {
+            throw new AlreadyTakenException("User already exists");
+        } else {
+            users.createUser(registerRequest);
+            return registerRequest;
+        }
+    }
+
+    public UserData login(UserData loginRequest) {
+        UserData user = users.getUser(loginRequest.username());
+        if (user == null || !Objects.equals(user.password(), loginRequest.password())) {
+            throw new InvalidLoginException("Incorrect Username or Password");
+        } else {
+            return user;
+        }
+    }
+
 }
