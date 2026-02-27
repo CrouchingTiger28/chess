abe57563c4814597b2dcf0b08bedd2a3a17eb1ac
CrouchingTiger28 <sabrinatj2004@gmail.com>
2026-02-26 19:55:21 -0700

    GameAccess and AuthAccess written

--- server/src/main/java/dataaccess/AuthAccess.java
+++ server/src/main/java/dataaccess/AuthAccess.java
@@ -1,6 +1,33 @@
 package dataaccess;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class AuthAccess {
 
+    private ArrayList<Model.AuthData> auths = new ArrayList<>();
+    public AuthAccess() {
 
+    }
+
+    public void createAuth(Model.AuthData data) {
+        auths.add(data);
+    }
+
+    public Model.AuthData getAuth(String authToken) {
+        for (Model.AuthData currentAuth : auths) {
+            if (Objects.equals(currentAuth.authToken(), authToken)) {
+                return currentAuth;
+            }
+        }
+        return null;
+    }
+
+    public void deleteAuth(String authToken) {
+        auths.removeIf(currentAuth -> Objects.equals(currentAuth.authToken(), authToken));
+    }
+
+    public void deleteAuths() {
+        auths = new ArrayList<>();
+    }
 }
--- server/src/main/java/dataaccess/GameAccess.java
+++ server/src/main/java/dataaccess/GameAccess.java
@@ -1,4 +1,49 @@
 package dataaccess;
 
+import Model.GameData;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class GameAccess {
+
+    private ArrayList<GameData> games = new ArrayList<>();
+    public GameAccess() {
+
+    }
+
+    public void createGame(GameData data) {
+        games.add(data);
+    }
+
+    public void updateGame(int gameID, String playercolor, String username) {
+        GameData game = getGame(gameID);
+        deleteGame(gameID);
+
+        if (Objects.equals(playercolor, "white")) {
+            createGame(new GameData(gameID, username, game.blackUsername(), game.gameName(), game.game()));
+        } else {
+            createGame(new GameData(gameID, game.whiteUsername(), username, game.gameName(), game.game()));
+        }
+    }
+
+    public GameData getGame(int gameID) {
+        for (GameData currentGame : games) {
+            if (currentGame.gameID() == gameID) {
+                return currentGame;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<GameData> listGames() {
+        return games;
+    }
+
+    public void deleteGame(int gameID) {
+        games.removeIf(currentGame -> currentGame.gameID() == gameID);
+    }
+
+    public void deleteGames() {
+        games = new ArrayList<>();
+    }
 }
