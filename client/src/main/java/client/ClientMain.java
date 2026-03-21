b2bde8c50574c7357a8eb86b2d22f73dd1f132bf
CrouchingTiger28 <sabrinatj2004@gmail.com>
2026-03-21 17:36:30 -0600

    joinGame Menu stuff

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,6 +1,8 @@
 package client;
 
 import chess.*;
+import model.GameData;
+import ui.DrawBoard;
 
 import java.util.InputMismatchException;
 import java.util.List;
@@ -10,6 +12,9 @@ public class ClientMain {
 
     private boolean loggedIn = false;
     private String authToken = null;
+    private ui.DrawBoard boardPen = new ui.DrawBoard();
+    private List<GameData> gameList = List.of();
+    private static ServerFacade serverFacade = new ServerFacade();
 
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
@@ -50,7 +55,7 @@ public class ClientMain {
     private void printPreloginMenu() {
         List<String> options = List.of("login", "register", "help", "quit");
         for (var i = 0; i < options.size(); i++) {
-            System.out.printf("\t%d. %s%n", i+1, options.get(i));
+            System.out.printf("\t%d. %s%n", i + 1, options.get(i));
         }
         System.out.print("\n");
     }
@@ -58,13 +63,13 @@ public class ClientMain {
     private void printPostloginMenu() {
         List<String> options = List.of("help", "logout", "create game", "list games", "play game", "observe game");
         for (var i = 0; i < options.size(); i++) {
-            System.out.printf("\t%d. %s%n", i+1, options.get(i));
+            System.out.printf("\t%d. %s%n", i + 1, options.get(i));
         }
         System.out.print("\n");
     }
 
     private void printHelp(Boolean loggedIn) {
-        String preLoginHelp  = "This is the help string before logging in.";
+        String preLoginHelp = "This is the help string before logging in.";
         String postLoginHelp = "This is the help string after logging in.";
 
         if (!loggedIn) {
@@ -75,19 +80,20 @@ public class ClientMain {
     }
 
     private boolean preloginMenuItem(int option) {
-        switch(option) {
+        switch (option) {
             case 1:
+                //login
                 loggedIn = true;
-
                 return true;
             case 2:
+                //register
                 loggedIn = true;
-
                 return true;
             case 3:
                 printHelp(false);
                 return true;
             case 4:
+                //quit
                 return false;
             default:
                 System.out.printf("%d %s", option, "is not a valid input. Select help (3) for additional assistance.\n");
@@ -96,7 +102,7 @@ public class ClientMain {
     }
 
     private void postloginMenuItem(int option) {
-        switch(option) {
+        switch (option) {
             case 1:
                 printHelp(true);
                 break;
@@ -111,12 +117,62 @@ public class ClientMain {
                 break;
             case 5:
                 //play game
+                joinGame();
                 break;
             case 6:
                 //observe game
                 break;
             default:
                 System.out.printf("%d %s", option, "is not a valid input. Select help (1) for additional assistance.\n");
+        }
+    }
+
+    private boolean joinGame() {
+        Scanner scanner = new Scanner(System.in);
+        int input;
+        boolean joinID = true;
+        boolean joinColor = true;
+        int gameID = 0;
+
+        while (joinColor) {
+            try {
+                input = scanner.nextInt();
+
+                if (joinID) {
+                    gameID = getGameID(input);
+                    if (gameID > 0) {
+                        joinID = false;
+                    }
+                }
+
+                if (input == 1) {
+                    //white
+                    joinColor = false;
+                    boardPen.drawWhite();
+                } else if (input == 2) {
+                    //black
+                    joinColor = false;
+                    boardPen.drawBlack();
+                } else {
+                    System.out.printf("%d %s", input, "is not a valid input.\n");
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println("Invalid input. Please try again.\n");
+                scanner.next();
+            }
+        }
+        return true;
+    }
+
+    private void updateGameList() {
+        gameList = serverFacade.listGames();
+    }
+
+    private int getGameID(int gameNumber) {
+        try {
+            return gameList.get(gameNumber).gameID();
+        } catch (IndexOutOfBoundsException ex) {
+            return 0;
         }
     }
 }
--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -1,4 +1,16 @@
 package ui;
 
 public class DrawBoard {
+
+    public DrawBoard() {
+
+    }
+
+    public void drawWhite() {
+        System.out.println("Drawing board from white perspective.");
+    }
+
+    public void drawBlack() {
+        System.out.println("Drawing board from black perspective.");
+    }
 }
